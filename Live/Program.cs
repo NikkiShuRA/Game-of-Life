d0b06e3c3470d18d92153da64bfc7330959c2750
NikkiShuRA <xak-bag-303@yandex.ru>
2025-06-02 02:35:01 +0500

    Изменение имён переменных (чтоб понятнее было)

--- Live/Program.cs
+++ Live/Program.cs
@@ -8,14 +8,14 @@ class Game
     };
     static string[,] placeUpdate = (string[,])place.Clone();
 
-    static int h = place.GetUpperBound(0) + 1;
-    static int w = place.Length / h;
+    static int arrayRow = place.GetUpperBound(0) + 1;
+    static int arrayColumn = place.Length / arrayRow;
 
     static void Main()
     {
+        Console.WriteLine($"row {arrayRow} column {arrayColumn}");
         while (true)
         {
-            Console.WriteLine($"r {h} c {w}");
             PrintArray();
             ArrayUpdate();
             place = (string[,])placeUpdate.Clone();
@@ -25,12 +25,9 @@ class Game
 
     public static void ArrayUpdate()
     {
-        int rows = place.GetUpperBound(0) + 1;
-        int columns = place.Length / rows;
-
-        for (int i = 0; i < rows; i++)
+        for (int i = 0; i < arrayRow; i++)
         {
-            for (int j = 0; j < columns; j++)
+            for (int j = 0; j < arrayColumn; j++)
             {
                 if (LiveChecker(i, j))
                 {
@@ -44,21 +41,21 @@ class Game
         }
     }
 
-    public static bool LiveChecker(int r, int c)
+    public static bool LiveChecker(int row, int column)
     {
         bool itLive = false;
-        if (place[r, c] == "+") itLive = true;
+        if (place[row, column] == "+") itLive = true;
 
         int countLiveObject = 0;
         for (int i = 0; i < 3; i++)
         {
             for (int j = 0; j < 3; j++)
             {
-                int newR = r - i + 1;
-                int newC = c - j + 1;
-                if ((newR, newC) == (r, c)) continue;
-                if ((newR < 0 || newR > h-1) || (newC < 0 || newC > w - 1)) continue;
-                if (place[newR, newC] == "+") countLiveObject++;
+                int newRow = row - i + 1;
+                int newColumn = column - j + 1;
+                if ((newRow, newColumn) == (row, column)) continue;
+                if ((newRow < 0 || newRow > arrayRow - 1) || (newColumn < 0 || newColumn > arrayColumn - 1)) continue;
+                if (place[newRow, newColumn] == "+") countLiveObject++;
             }
         }
 
@@ -75,10 +72,10 @@ class Game
     public static void PrintArray()
     {
         PrintBox();
-        for (int i = 0; i < h; i++)
+        for (int i = 0; i < arrayRow; i++)
         {
             Console.Write("|");
-            for (int j = 0; j < w; j++)
+            for (int j = 0; j < arrayColumn; j++)
             {
                 Console.Write($"{place[i, j]}");
             }
@@ -90,7 +87,7 @@ class Game
     public static void PrintBox()
     {
         Console.Write("+");
-        for (int column = 0; column < w; column++)
+        for (int column = 0; column < arrayColumn; column++)
         {
             Console.Write("-");
         }
